b81477553f0e4a9e4dda65142cd6d4ae3d364b52
roconn <roconn25@gmail.com>
2022-03-31 12:57:43 +0100

    added to comments.js model

---
+++ models/Comments.js
@@ -0,0 +1,22 @@
+const { Model, DataTypes } = require('sequelize')
+
+const sequelize = require('../config/connection')
+
+class Comments extends Model {}
+
+Comments.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    comment: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    sequelize,
+    modelName: "comments"
+}) 
+
+module.exports = Comments
