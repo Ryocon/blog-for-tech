86c47b7672135a1bf3ea1f4512c17b1e1b62b1c7
roconn <roconn25@gmail.com>
2022-04-04 20:26:53 +0100

    postRoutes completed

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -4,7 +4,7 @@ const { Post } = require('../../models/')
 
 const withAuth = require('../../utils/auth')
 
-
+// display all posts
 router.get('/', async (req, res) => {
     try {
       const postData = await Post.findAll({});
@@ -15,6 +15,7 @@ router.get('/', async (req, res) => {
     }
   });
 
+// create a post
 router.post('/', withAuth, async, (req, res) =>{
     try {
         const postData = await Post.create({
@@ -28,3 +29,36 @@ router.post('/', withAuth, async, (req, res) =>{
         res.status(400).json(err)
     }
 })
+
+// edit a post
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.update(req.body, { where: { id: req.body.id } })
+        res.status(200).render('dashboard', { logged_in: req.session.logged_in })
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+// delete a post
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        })
+
+        if (!postData) {
+            res.status(404).json({ message: 'No post with this id found!' })
+            return
+        }
+
+        res.status(200).json(postData)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+module.exports = router
